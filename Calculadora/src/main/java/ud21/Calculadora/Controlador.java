279665238ab5279bab42c99048bce9815e4227f3
David Marcos <david.13254@gmail.com>
2022-08-07 14:17:41 +0200

    definir metodos Controlador

--- Calculadora/src/main/java/ud21/Calculadora/Controlador.java
+++ Calculadora/src/main/java/ud21/Calculadora/Controlador.java
@@ -1,16 +1,20 @@
 package ud21.Calculadora;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Controlador {
+	
+	VentanaCalc ventanaCalc;
 
 	public Controlador() {
 		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					VentanaCalc frame = new VentanaCalc();
-					frame.setVisible(true);
+					VentanaCalc ventanaCalc = new VentanaCalc();
+					ventanaCalc.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -18,5 +22,44 @@ public class Controlador {
 		});
 	}
 	
+	private void actionBtnCE() {
+		/*ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			
+			}
+		}*/
+	}
+	
+	private void actionBtnBorrar() {
+		/*ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent arg0) {
+		
+			}
+		}*/
+	}
+	
+	private void actionBtnNums() {
+		/*ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent arg0) {
+		
+			}
+		}*/
+	}
+	
+	private void actionBtn0() {
+		/*ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent arg0) {
+		
+			}
+		}*/
+	}
+	
+	private void actionBtnComa() {
+		/*ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent arg0) {
+		
+			}
+		}*/
+	}
 	
 }
