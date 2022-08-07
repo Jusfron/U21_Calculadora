dfb2e6ee19d03b2095584a9f57e16c79fae6dd09
Joana <joanajanefdez@gmail.com>
2022-08-07 21:55:43 +0200

    DivisaTest.java acabado

--- Calculadora/src/test/java/ud21/Calculadora/DivisaTest.java
+++ Calculadora/src/test/java/ud21/Calculadora/DivisaTest.java
@@ -2,33 +2,56 @@ package ud21.Calculadora;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class DivisaTest {
 
+
+class DivisaTest {
+	
+	Divisa divisa;
+
+	@BeforeEach
+	public void before() {
+		String pais = "Reino Unido";
+		String nombre = "Libra";
+		String simbolo = "£";
+		Double factorConv = 0.8282;	
+		divisa = new Divisa(pais,nombre,simbolo,factorConv);
+	}
+	
 	@Test
 	void testDivisa() {
-		fail("Not yet implemented");
+		Divisa divisa2 = new Divisa("Reino Unido","Libra","£",0.8282);
+		assertNotNull(divisa2);
 	}
 
 	@Test
 	void testGetNombre() {
-		fail("Not yet implemented");
+		String nombreDiv = divisa.getNombre();
+		String nombreDivEsperado = "Libra";
+		assertEquals(nombreDivEsperado,nombreDiv);
 	}
 
 	@Test
 	void testGetSimbolo() {
-		fail("Not yet implemented");
+		String simbolDiv = divisa.getSimbolo();
+		String simbolDivEsperado = "£";
+		assertEquals(simbolDivEsperado,simbolDiv);
 	}
 
 	@Test
 	void testGetFactorConversion() {
-		fail("Not yet implemented");
+		Double FactorDiv = divisa.getFactorConversion();
+		Double FactorDivEsperado = 0.8282;
+		assertEquals(FactorDivEsperado,FactorDiv);
 	}
 
 	@Test
 	void testGetPais() {
-		fail("Not yet implemented");
+		String paisDiv = divisa.getPais();
+		String paisDivEsperado = "Reino Unido";
+		assertEquals(paisDivEsperado,paisDiv);
 	}
 
 }
