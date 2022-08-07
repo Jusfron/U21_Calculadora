3e412182bbfa1f14fab39ec7be3a1f15003068a4
David Marcos <david.13254@gmail.com>
2022-08-07 13:59:25 +0200

    terminal clase Divisa

--- Calculadora/src/main/java/ud21/Calculadora/Divisa.java
+++ Calculadora/src/main/java/ud21/Calculadora/Divisa.java
@@ -1,5 +1,23 @@
 package ud21.Calculadora;
 
 public class Divisa {
+	
+	private String nombre;
+	private Double factorConversion;
+	
+	public Divisa(String nombre, Double factorConversion) {
+		this.nombre = nombre;
+		this.factorConversion = factorConversion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public Double getFactorConversion() {
+		return factorConversion;
+	}
+	
+	
 
 }
