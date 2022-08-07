3c6c4bd904cef21bb340cccd132e3cbdec64729a
David Marcos <david.13254@gmail.com>
2022-08-07 14:03:37 +0200

    esqueleto clase Calculadora

--- Calculadora/src/main/java/ud21/Calculadora/Calculadora.java
+++ Calculadora/src/main/java/ud21/Calculadora/Calculadora.java
@@ -1,5 +1,18 @@
 package ud21.Calculadora;
 
+import java.util.ArrayList;
+
 public class Calculadora {
+	
+	ArrayList<Divisa> divisas;
+	
+	public Calculadora() {
+		
+	}
+	
+	public Double conversion(Double cantidad, Divisa divisaInput, Divisa divisaOutput) {
+		
+		return cantidad;
+	}
 
 }
