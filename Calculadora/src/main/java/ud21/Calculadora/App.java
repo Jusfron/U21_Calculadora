850c889036d42f0d3b462fa1512a747729d2527c
ionela vladasel <ionelavladasel2710@gmail.com>
2022-08-07 18:01:18 +0200

    organize

--- Calculadora/src/main/java/ud21/Calculadora/App.java
+++ Calculadora/src/main/java/ud21/Calculadora/App.java
@@ -14,5 +14,7 @@ public class App{
 	public static void main(String[] args) {
 		Controlador control = new Controlador();
 	}   
+	
+	//
 
 }
--- Calculadora/src/main/java/ud21/Calculadora/Controlador.java
+++ Calculadora/src/main/java/ud21/Calculadora/Controlador.java
@@ -5,20 +5,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Controlador {
-	
+
 	VentanaCalc ventanaCalc;
 	Calculadora calculadora;
 
 	public Controlador() {
-		
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					VentanaCalc ventanaCalc = new VentanaCalc();
 					ventanaCalc.setVisible(true);
-					
+
 					calculadora = new Calculadora();
-					
+
 					actionBtnCE();
 					actionBtnBorrar();
 					actionBtnNums();
@@ -30,46 +30,69 @@ public class Controlador {
 			}
 		});
 
-	}		
-	
+	}
+
 	private void actionBtnCE() {
-		/*ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			
-			}
-		}*/
+		/*
+		 * ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
+		 * public void actionPerformed(ActionEvent arg0) {
+		 * 
+		 * } }
+		 */
 	}
-	
+
 	private void actionBtnBorrar() {
-		/*ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent arg0) {
-		
-			}
-		}*/
+		/*
+		 * ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
+		 * public void actionPerformed(ActionEvent arg0) {
+		 * 
+		 * } }
+		 */
 	}
-	
+
 	private void actionBtnNums() {
-		/*ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent arg0) {
-		
-			}
-		}*/
+		/*
+		 * ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
+		 * public void actionPerformed(ActionEvent arg0) {
+		 * 
+		 * } }
+		 */
 	}
-	
+
 	private void actionBtn0() {
-		/*ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent arg0) {
-		
-			}
-		}*/
+		/*
+		 * ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
+		 * public void actionPerformed(ActionEvent arg0) {
+		 * 
+		 * } }
+		 */
 	}
-	
+
 	private void actionBtnComa() {
-		/*ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent arg0) {
-		
-			}
-		}*/
+		/*
+		 * ventanaCalc.getBtnAnadirPalabra().addActionListener(new ActionListener() {
+		 * public void actionPerformed(ActionEvent arg0) {
+		 * 
+		 * } }
+		 */
+
 	}
-	
+
+	private void actionCmbInput() {
+		ventanaCalc.getCmbInput().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String divisaSeleccionada = ventanaCalc.getCmbInput().getSelectedItem().toString();
+
+			}
+		});
+	}
+
+	private void actionCmbOutput() {
+		ventanaCalc.getCmbOutput().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String divisaSeleccionada = ventanaCalc.getCmbOutput().getSelectedItem().toString();
+
+			}
+		});
+	}
 }
